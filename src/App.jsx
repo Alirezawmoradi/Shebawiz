8e7cb3f55506491d1096143914866490ea90c1a0
Alirezaw <Alirezawmoradi@gmail.com>
2024-01-30 01:34:58 +0330

    tailwind css config added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ function App() {
 
     return (
         <>
-
+            <div className='flex justify-center font-bold items-center text-3xl mt-10'>
+                Shebawiz Application
+            </div>
         </>
     )
 }
